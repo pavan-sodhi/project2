09d1b8ada7ce5798ac8d0f537ade1d05aa2a0f16
Pavandeep Singh <pavan.sodhi@gmail.com>
2017-12-09 14:22:40 -0500

    fixed file

--- database.php
+++ database.php
@@ -1,15 +1,8 @@
 <?php
-<<<<<<< HEAD
+
 define('DATABASE', 'ps355');
 define('USERNAME', 'ps355');
 define('PASSWORD', 'JB8PJj5M');
 define('CONNECTION', 'sql1.njit.edu');
 
 ?>
-=======
-
-define('DATABASE', 'kwilliam');
-define('USERNAME', 'kwilliam');
-define('PASSWORD', 'USracpEto');
-define('CONNECTION', 'sql2.njit.edu');
->>>>>>> added login
