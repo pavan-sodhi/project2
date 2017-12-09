58d03f55b094474290514fe679101fa775e8a479
Pavandeep Singh <pavan.sodhi@gmail.com>
2017-12-09 14:00:49 -0500

    changed database cred

--- database.php
+++ database.php
@@ -1,7 +1,15 @@
 <?php
+<<<<<<< HEAD
 define('DATABASE', 'ps355');
 define('USERNAME', 'ps355');
 define('PASSWORD', 'JB8PJj5M');
 define('CONNECTION', 'sql1.njit.edu');
 
 ?>
+=======
+
+define('DATABASE', 'kwilliam');
+define('USERNAME', 'kwilliam');
+define('PASSWORD', 'USracpEto');
+define('CONNECTION', 'sql2.njit.edu');
+>>>>>>> added login
