6989c979319827ac35f82e60585653d0527748b7
Pavandeep Singh <pavan.sodhi@gmail.com>
2017-12-09 12:45:50 -0500

    changes

--- pages/homepage.php
+++ pages/homepage.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
 
-    <title>Final Project</title>
-    <meta name="description" content="IS601 Fall Final Project">
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
@@ -17,28 +17,31 @@
 
 <body>
 
-<?php
-//this is how you print something
 
-echo '<h1>' . $data['site_name']  . '</h1>'
+<h1>
+    <?php
+    //this how to print some data;
+    echo $data['site_name'];
+    ?> </h1>
 
-?>
+<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
+<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
 
-<form action="index.php" method="post">
+<form action="index.php?page=accounts&action=login" method="POST">
 
     <div class="container">
         <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required>
+        <input type="text" placeholder="Enter Username" name="email" required>
 
         <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        <input type="password" placeholder="Enter Password" name="password" required>
 
         <button type="submit">Login</button>
     </div>
 
 
 </form>
-
+<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
 
 
 <script src="js/scripts.js"></script>
